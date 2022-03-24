fc37312277637f1f503d603b502e1f3e7c235e4d
keithjsb <37438005+keithjsb@users.noreply.github.com>
2022-03-24 17:12:50 +0800

    test

--- js/script.js
+++ js/script.js
@@ -3,9 +3,16 @@ function placeholderFunction() {
 }
 
 $(document).ready(function () {
+	let x = "";
+
+	if (screen.width >= 600)
+		x = "longer";
+	else
+		x = "shorter";	
+
 	$(".txtYear").text(getYear());
 	$("#resolution1").text("Width: " + screen.width + ", Length: " + screen.height);
-	$("#resolution2").text("Width: " + (screen.width * 0.7) + ", Length: " + (screen.height * 0.7));
+	$("#resolution2").text("Width: " + (screen.width * 0.7) + ", Length: " + (screen.height * 0.7) + " " + x);
 	console.log(getYear());
 });
 
