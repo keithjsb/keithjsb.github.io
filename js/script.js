02b032c0ca17ccb7e64bcd8e505db3185ce182c9
keithjsb <37438005+keithjsb@users.noreply.github.com>
2022-03-24 15:34:35 +0800

    202203241534

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 function placeholderFunction() {
 	document.getElementById("placeholder").innerHTML = "Another placeholder here.";
 }
+
+$(document).ready(function () {
+	$(".txtYear").text(getYear());
+	$("#resolution1").text("Width: " + screen.width + ", Length: " + screen.height);
+	$("#resolution2").text("Width: " + (screen.width * 0.7) + ", Length: " + (screen.height * 0.7));
+	console.log(getYear());
+});
+
+function getDate(){
+	return new Date();
+}
+
+function getYear(){
+	return getDate().getFullYear();
+}
